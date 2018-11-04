c619b0c6c156a0de523e1f10f85d7b1e703d4198
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-04 16:49:40 +0000

    adds partial for chefs

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,4 +1,5 @@
 class ChefsController < ApplicationController
+  has_secure_password
   def new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :ingredients
   resources :dishes
   resources :chefs
+  root 'applcation#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
