25e6c83a2b5bbabb3d61b01bec773034e48286b8
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-05 04:00:38 +0000

    builds chef controller

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,20 +1,39 @@
 class ChefsController < ApplicationController
   has_secure_password
-  def new
-  end
-
-  def create
-  end
+  before_action :set_chef, only: [:show, :edit, :update, :destroy]
 
   def show
   end
 
+  def new
+    @chef = Chef.new
+  end
+
+  def create
+    @chef = Chef.new(chef_params)
+    if @chef.save
+      session[:chef_id] = @chef.id
+      redirect_to chef_path(@chef)
+    else
+      render :new
+    end
+  end
+
   def update
+    if @chef.update(chef_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private
+  def set_chef
+    @chef = Chef.find(params[:id])
+  end
+
   def chef_params
-    params.require(:user).permit(
+    params.require(:chef).permit(
     :name,
     :password
     )
--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,8 +2,15 @@ class DishesController < ApplicationController
   def new
   end
   def create
+    @dish = Dish.new
   end
 
 
   private
+    def dish_params
+      params.require(:dish).permit(
+        :name,
+        :cook_time
+      )
+    end
 end
