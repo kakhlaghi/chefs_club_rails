94a51bf77183beecbbb22ea98c41bdb47a1ab506
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-05 03:40:11 +0000

    adds application controller methods for login, adds show methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+    before_action :current_user
+    before_action :require_logged_in, except: [:new, :create, :home]
+
+    def logged_in?
+      !!current_user
+    end
+
+    private
+      def require_logged_in
+        redirect_to root_path unless logged_in?
+      end
+
+      def current_user
+        @current_user ||= Chef.find(session[:chef_id]) if session[:chef_id]
+      end
+      helper_method :current_user
 end
--- db/migrate/20181104013244_create_ingredients.rb
+++ db/migrate/20181104013244_create_ingredients.rb
@@ -2,7 +2,6 @@ class CreateIngredients < ActiveRecord::Migration[5.2]
   def change
     create_table :ingredients do |t|
       t.string :name
-      t.string :type
       t.belongs_to :chef, foreign_key: true
       t.belongs_to :dish, foreign_key: true
 
