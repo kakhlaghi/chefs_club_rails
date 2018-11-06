be9194f27912348ef6a228d3798bb3245d77b172
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-06 19:55:23 +0000

    adds edit view and form for dishes, accepts nested atrributes add for dishes

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,7 @@ class Dish < ActiveRecord::Base
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
   belongs_to :chef
+  accepts_nested_attributes_for :ingredients
   
   def chef_name
     self.try(:chef).try(:name)
