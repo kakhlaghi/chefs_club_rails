b9b55984995bf589563ed070b081badc639b3b53
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-06 18:34:00 +0000

    adds chef to dish migration, and chef name methods to dish model

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,13 @@ class Dish < ActiveRecord::Base
   has_many :dish_ingredients
   has_many :ingredients, through: :dish_ingredients
   belongs_to :chef
+  
+  def chef_name
+    self.try(:chef).try(:name)
+  end
+
+  def chef_name=(name)
+    chef = Chef.find_or_create_by(name: name)
+    self.chef = chef
+  end
 end
