e06d8b4248b3ad66c7db99efd968596596e6a918
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-07 01:39:09 +0000

    fixes add new dish path from chef profile page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
   def new
+    @chef = Chef.new
+    @chefs = Chef.all
   end
   def create
     @chef = Chef.find_by(name: params[:chef][:name])
  if @chef && @chef.authenticate(params[:chef][:password])
    session[:user_id] = @chef.id
-   redirect_to user_path(@chef), notice: "Welcome back, chef!"
+   redirect_to chef_path(@chef), notice: "Welcome back, chef!"
  else
    redirect_to signin_path
  end
