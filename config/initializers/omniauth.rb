9625110a07a787e49c75f9028358429fc8b458ed
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-14 23:01:22 +0000

    add client id and secret to file while I figure out .env permanence

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user:email,user:follow"
+  provider :github, 'c2bfec121283821adffa', '6b0316972cf71fcb10a72970ff54defb1fc35ae3', scope: "user:email,user:follow"
 end
