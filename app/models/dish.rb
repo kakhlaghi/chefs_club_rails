9b967c014dcbbc07d19814d633507e0516d4e880
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-15 15:46:23 +0000

    works on nested form implementation. untested

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -7,7 +7,7 @@ class Dish < ActiveRecord::Base
   scope :more_than_30, -> {where('cook_time > 30')}
   scope :less_than_60, -> {where('cook_time <= 60')}
   scope :more_than_60,-> {where('cook_time > 60')}
-  accepts_nested_attributes_for :ingredients
+
   accepts_nested_attributes_for :dish_ingredients
   validates :name, presence: {message:"Your dish needs a name!"}
   validates :cook_time, numericality: { greater_than: 0 }
@@ -21,18 +21,7 @@ class Dish < ActiveRecord::Base
     self.chef = chef
   end
 
-  def ingredients_attributes=(ingredients_attributes)
-    ingredients_attributes.each do |i, ingredient_attributes|
-        #category = Category.find_or_create_by(category_attributes)
-        if ingredient_attributes[:name].present?
-          ingredient = Ingredient.find_or_create_by(name: ingredient_attributes[:name])
-          if !self.ingredients.include?(ingredient)
-          #self.categories << category is inefficient
-            self.ingredients.build(ingredient_attributes) #avoids an extra step DOES EXACTLY what we want and nothing else 53:46
-          end
-        end
-      end
-    end #end ingredient_attributes
+   #end ingredient_attributes
 
   #def dish_ingredients_attributes=(dish_ingredients_attributes)
   #  dish_ingredients_attributes.each do |i, dish_ingredient_attributes|
--- app/models/dish_ingredient.rb
+++ app/models/dish_ingredient.rb
@@ -1,6 +1,19 @@
 class DishIngredient < ActiveRecord::Base
   belongs_to :dish
   belongs_to :ingredient, required: false
- 
+  accepts_nested_attributes_for :ingredients
+
+  def ingredients_attributes=(ingredients_attributes)
+    ingredients_attributes.each do |i, ingredient_attributes|
+        #category = Category.find_or_create_by(category_attributes)
+        if ingredient_attributes[:name].present?
+          ingredient = Ingredient.find_or_create_by(name: ingredient_attributes[:name])
+          if !self.ingredients.include?(ingredient)
+          #self.categories << category is inefficient
+            self.ingredients.build(ingredient_attributes) #avoids an extra step DOES EXACTLY what we want and nothing else 53:46
+          end
+        end
+      end
+    end
 
 end
