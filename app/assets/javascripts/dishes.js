3b1206efb9173e97b82cb46f7babd620b1fba54c
kakhalghi <kevin.akhlaghi@gmail.com>
2018-12-07 05:57:17 -0800

    hijacks ingredient submit from show page, deletes coffee file for dishes

--- app/assets/javascripts/dishes.js
+++ app/assets/javascripts/dishes.js
@@ -1,49 +1,46 @@
-
-//form to dynamically add ingredients without having to change the page
-$(function(){
-  //function listenForNewIngredient()
-
-  
+$(function () {
+	listenForIngredientIndex()
+	//listenForChefsIndex()
+	newIngredient()
 });
 
-//listeners
-function listenForNewIngredient(){
-    newIngredient()
+// listeners.....
+function listenForIngredientIndex() {
+	// when something is clicked
+	// fetchDishes()
+	$()
 }
 
-
-
-/*function getAllDishes(){
-    const allDishes = 
-  }*/
-
-function newIngredient(){
-  $('input#new_ingredient').on('click', function(e){
-    e.preventDefault();
-    let qty = document.getElementById('#dish_dish_ingredients_attributes_0_quantity')
-    let name = document.getElementById('#dish_dish_ingredients_attributes_0_ingredients_name')
-    $.ajax({
-        type:"PATCH",
-        url: this.action,
-        data: $(this).serialize,
-        success: function(response){
-            let $ul = $("div.ingredients ul")
-            $ul.append(response);
-        }
-    }) 
-  });
+// fetchers .....
+function fetchDishes() {
+	// ajax   '/chefs/:id/dishes
 }
 
-class Menu {
-    constructor(dishes, time){
-        this.dishes=dishes;
-        this.cook_time=cook_time;
-    }
+function fetchChefs() {
+	// ajax   '/chefs
 }
 
-/*Menu.prototype.sumCookTime = function(){
-    Menu
+function fetchChefDetails() {
+	// ajax   '/chefs/:id
 }
 
-*/
+function newIngredient() {
+	// $('#new_ingredients').on("submit", function (event) {
+	$('input#new_ingredient').on("click", function (event) {
+		console.log("hit")
+		console.log(event)
+		let qty = $('#dish_dish_ingredients_attributes_0_quantity').val()
+		let name = $('#dish_dish_ingredients_attributes_0_ingredients_name').val()
 
+		event.preventDefault();
+		$.ajax({
+			type: "PATCH",
+			url: this.action,
+			data: $(this).serialize,
+			success: function (response) {
+				let $ul = $("div.ingredients ul")
+				$ul.append(response);
+			}
+		})
+	});
+}
