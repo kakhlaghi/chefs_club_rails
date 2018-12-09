13cbc566ecb7bffd28b9e2c834bee84cac866f0b
kakhalghi <kevin.akhlaghi@gmail.com>
2018-12-08 17:57:18 -0800

    works on Favorites class

--- app/assets/javascripts/chefs.js
+++ app/assets/javascripts/chefs.js
@@ -4,28 +4,20 @@ $(function () {
 	//listenForIngredientIndex()
 	//listenForFetchChefs()
 	//newIngredient()
-	if(Favorites == 'undefined'){
-		const chefFav = new Favorites();
-	}
+	
 });
 
 function favoritesManager(){
     const baseURL = 'localhost:3000'
 	const url = window.location.href + '.json';
-
 	let postData = {
 		dish: document.getElementsByClassName('.dishName')
 	}
-	debugger
 	//get the data
-	fetch(url,{
-		method: 'POST',
-		body: JSON.stringify(postData)
-	})
+	fetch(url)
 		  .then(response=>response.json())
-		  .then(json=>Favorites.addToFavorites(json))
-		  .then(console.log(Favorites))
-
+		  .then(json=>Favorites(json).call(chefFavorite))
+		  console.log(chefFavorite)
 }
 
 class Favorites{
