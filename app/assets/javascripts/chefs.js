1f70a192e4b9f0962c1d8283fcf00675748a30f7
kakhalghi <kevin.akhlaghi@gmail.com>
2018-12-09 12:45:26 -0800

    stores json into favoriteStore. right now favoriteStore is a Global Variable

--- app/assets/javascripts/chefs.js
+++ app/assets/javascripts/chefs.js
@@ -1,14 +1,17 @@
 //favorites
-let favoriteStore = {faveDishes: []} 
 
 $(function () {
 	//listenForIngredientIndex()
 	//listenForFetchChefs()
 	//newIngredient()
+
 });
 
 
-function favoritesManager(){
+let favoriteStore = {faveDishes: []} 
+
+function favoritesFetcher(){
+	let chefFavorite = new Favorites()
     const baseURL = 'localhost:3000'
 	const url = window.location.href + '.json';
 	let postData = {
@@ -19,16 +22,16 @@ function favoritesManager(){
 		  .then(response=>response.json())
 		  .then(json=> chefFavorite.addToFavorites(json))
 		  console.log(chefFavorite)
-}
-
-class Favorites{
-	constructor(){
-		this.array = [];
-		favoriteStore.faveDishes.push(this)
 	}
-}
 
-Favorites.prototype.addToFavorites = function(dish){
-	this.array.push(dish)
-};
+	class Favorites{
+		constructor(){
+			this.array = [];
+			favoriteStore.faveDishes.push(this)
+		}
+	}
+
+	Favorites.prototype.addToFavorites = function(dish){
+		this.array.push(dish)
+	};
 
